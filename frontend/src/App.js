8c9027fb5d5587406f24283cf5b1a6a7bf16634f
Anika Choudhury <anikachoudhury@XR2039R4XC.local>
2022-05-23 14:36:38 +0100

    sign in page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './images/ADlogo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="login-container">
+        <img src={logo}></img>
+
+        <h1>Sign In</h1>
+
+        <label htmlFor="email">Email Address</label>
+        <input type="text" id="email" placeholder="Example: john.smith@and.digital"></input>
+
+        <button>Continue</button>
+      </div>
+
     </div>
   );
 }
