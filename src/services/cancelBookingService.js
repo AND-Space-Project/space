ae960eb637f0a4fa60a08ccded247749593b8e4d
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2022-05-23 13:02:36 +0100

    Added cancel booking service

---
+++ src/services/cancelBookingService.js
@@ -0,0 +1,60 @@
+const { Connection, Request, TYPES } = require("tedious");
+const GetConnection = require('./initiateDbConnection.js');
+const MURRAY_ID = 1;
+
+module.exports = function CancelBooking(bookingDate, email) {
+    connection = GetConnection();
+    connection.connect(function(err) {
+        if (err) {
+          console.log('Connection Failed!');
+          throw err;
+        }
+
+        FindClubDay();
+    });
+
+    function FindClubDay() {
+        var ClubDayId = -1;
+        var request = new Request(
+            'SELECT TOP 1 @Id=ClubDayId FROM [dbo].[ClubDays] WHERE Date = @bookingDate AND ClubId = @clubId',
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                } else {
+                    Delete(ClubDayId);
+                }
+            }
+        );
+
+        request.addParameter('bookingDate', TYPES.Date, bookingDate);
+        request.addParameter('clubId', TYPES.Int, MURRAY_ID);
+        request.addOutputParameter('Id', TYPES.Int);
+
+        request.on('returnValue', (paramName, value) => {
+            ClubDayId = value;
+        });
+        
+        connection.execSql(request);
+    }
+
+    function Delete(ClubDayId) {
+        var request = new Request(
+            'DELETE FROM [dbo].[Bookings] WHERE ClubDayId = @clubDayId AND Email = @email',
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+
+                return;
+            }
+        );
+
+        request.addParameter('clubDayId', TYPES.Int, ClubDayId);
+        request.addParameter('email', TYPES.VarChar, email);
+
+        connection.execSql(request);
+    }
+
+    connection.close();
+    return;
+}
