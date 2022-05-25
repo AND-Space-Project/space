70eb6373eef3ca2beaf67cb3da6635a70a7b7552
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2022-05-25 12:13:37 +0100

    Add UserInfo class

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,14 +4,20 @@ import './styling/Login.css';
 import logo from '../images/ADlogo.png';
 import spacelogo from '../images/SPACE-Logo3.png';
 import { ParseEmail } from '../services/nameParser.js';
+import UserInfo from '../services/UserInfo';
 
 function Login() {
     const [email, setEmail] = useState('');
     const fullName = ParseEmail(email);
+    const [clubId, setClubId] = useState(1);
 
     const handleSubmit = (e) => {
-        console.log(fullName)
-        console.log(email);
+        UserInfo.setEmail(email);
+        UserInfo.setClubId(clubId);
+        UserInfo.setFullName(fullName);
+        console.log(UserInfo.getEmail());
+        console.log(UserInfo.getClubId());
+        console.log(UserInfo.getFullName());
     }
 
     return(
@@ -30,8 +36,9 @@ function Login() {
                     onChange={ e => setEmail(e.target.value)}>
                 </input>
 
-                <select name="Club" id="club">
+                <select name="Club" value={clubId} onChange={ e => setClubId(e.target.value)}>
                     <option value='1'>Club Murray</option>
+                    <option value='2'>Club Dekker</option>
                 </select>
 
                 <button onClick={handleSubmit()}>Continue</button>
