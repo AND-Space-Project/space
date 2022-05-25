3cc4cec997667aa25a046941197c5a4de03b9734
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2022-05-25 18:51:16 +0100

    Full clubs API

--- backend/routes/clubs.js
+++ backend/routes/clubs.js
@@ -11,4 +11,31 @@ router.get('/', async function(req, res, next) {
   }
 });
 
+router.post('/', async function(req, res, next) {
+    try {
+        res.json(await clubs.create(req.body));
+    } catch (err) {
+        console.error(`Error while creating club`, err.message);
+        next(err);
+    }
+});
+
+router.put('/:id', async function(req, res, next) {
+    try {
+      res.json(await clubs.update(req.params.id, req.body));
+    } catch (err) {
+      console.error(`Error while updating club`, err.message);
+      next(err);
+    }
+});   
+
+router.delete('/:id', async function(req, res, next) {
+    try {
+      res.json(await clubs.remove(req.params.id));
+    } catch (err) {
+      console.error(`Error while deleting club`, err.message);
+      next(err);
+    }
+  });
+
 module.exports = router;
--- backend/services/clubs.js
+++ backend/services/clubs.js
@@ -16,6 +16,55 @@ async function getMultiple(page = 1){
   }
 }
 
+async function create(club){
+  const result = await db.query(
+    `INSERT INTO Clubs 
+    (ClubId, Name, DefaultNumDesks, DateCreated) 
+    VALUES 
+    (${club.ClubId}, ${club.Name}, ${club.DefaultNumDesks}, ${new Date().toISOString().slice(0, 19).replace('T', ' ')})`
+  );
+
+  let message = 'Error in creating club';
+
+  if (result.affectedRows) {
+    message = 'Club created successfully';
+  }
+
+  return {message};
+}
+
+async function update(id, club){
+  const datenow = new Date().toISOString().slice(0, 19).replace('T', ' ');
+  const result = await db.query(
+    `UPDATE Clubs SET Name="${club.name}", DefaultNumDesks=${club.DefaultNumDesks}, DateModified="${datenow}" WHERE ClubId=${id}` 
+  );
+
+  let message = 'Error in updating club';
+
+  if (result.affectedRows) {
+    message = 'Club updated successfully';
+  }
+
+  return {message};
+}
+
+async function remove(id){
+  const result = await db.query(
+    `DELETE FROM clubs WHERE id=${id}`
+  );
+
+  let message = 'Error in deleting club';
+
+  if (result.affectedRows) {
+    message = 'Club deleted successfully';
+  }
+
+  return {message};
+}
+
 module.exports = {
-  getMultiple
+  getMultiple,
+  create,
+  update,
+  remove
 }
