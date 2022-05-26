be99a24cb35dd04fec295e257f9a4d15d6da3e23
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2022-05-26 14:52:01 +0100

    Max connections = 10

--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,7 @@ const config = {
       user: "spacedb@space-db-mysql",
       password: "(password)",
       database: "space-db-mysql",
+      connectionLimit: 10
     },
     listPerPage: 10,
   };
